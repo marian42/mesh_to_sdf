d51bab479fb1150f25bbd59fcfbfceb16ebf994e
Marian <mail@marian42.de>
2020-01-18 19:00:08 +0100

    Add license

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
